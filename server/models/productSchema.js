8cd0145670b454611bc04ccea20303e03d5e7e05
unknown <navjot1561.cse19@chitkara.edu.in>
2022-03-05 19:26:25 +0530

    connections and seeded data

---
+++ server/models/productSchema.js
@@ -0,0 +1,27 @@
+const mongoose=require("mongoose");
+
+
+const productSchema=new mongoose.Schema({
+name:{
+    type:String,
+    trim:true,
+    required:true,
+},
+price:{
+    type:Number,
+    min:0,
+},
+img:{
+    type:String,
+    trim:true,
+    default:"/images/product.jpg",
+},
+desc:{
+    type:String,
+    trim:true,
+}
+});
+
+
+mongoose.model("Product",productSchema);
+// module.exports=Product
---
+++ server/routes/productRoutes.js
@@ -0,0 +1,53 @@
+const express=require("express");
+const router=express.Router();
+const mongoose=require("mongoose");
+// const Product=require("../models/productSchema");
+const Product=mongoose.model("Product")
+
+router.get("/",async(req,res)=>{
+try{
+const product=await Product.find({});
+res.json({product});
+}
+catch(err){
+console.log("not getting all posts "+err);
+}
+
+})
+
+
+
+router.get("/",async(req,res)=>{
+    try{
+    const product=await Product.find({});
+    res.json({product});
+    }
+    catch(err){
+    console.log("not getting all posts "+err);
+    }
+    
+    });
+
+
+
+router.post("/createPosts",async (req,res)=>{
+    try{
+const {name,price,img,desc}=req.body;
+if(!name || !price || !img || !desc){
+    return res.status(422).json({error:"please add all the fields"});
+
+}
+const product=new Product({
+    name,price,img,desc
+});
+const result=await product.save();
+   res.json({posted:result});
+    }
+    catch(err){
+console.log("unable to post "+err);
+    }
+
+})
+
+
+module.exports=router 
