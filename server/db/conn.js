99b6d93059f97f1d37006bb01da4ed8f3ba730c7
unknown <navjot1561.cse19@chitkara.edu.in>
2022-06-26 12:57:30 +0530

    some commit

--- server/db/conn.js
+++ server/db/conn.js
@@ -4,8 +4,8 @@ dotenv.config({path:"./config/config.env"});
 console.log("ayi kya")
 mongoose.connect(process.env.DATABASE,{
     useNewUrlParser:true,
-    useUnifiedTopology:true,
-}).then(()=>{
+    useUnifiedTopology:true, 
+}).then(()=>{  
     console.log("connection is successFull");
 }).catch((err)=>{
     console.log("unable to connect "+err);
